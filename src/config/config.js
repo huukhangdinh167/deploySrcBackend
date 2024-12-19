5dc070af1a04d3c5defdc24e0bdf39e0600e3d19
Tran Minh Thuan <tranminhthuan7230@gmail.com>
2024-12-20 00:09:51 +0700

    config

--- src/config/config.js
+++ src/config/config.js
@@ -34,7 +34,7 @@ const path = require('path');
 
 module.exports = {
   development: {
-    username:  process.env.DB_USER,
+    username: process.env.DB_USER,
     // password: DB_PASSWORD,
     // database: DB_NAME,
     host: process.env.DB_HOST || "127.0.0.1",
@@ -56,17 +56,47 @@ module.exports = {
     }
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    // password: DB_PASSWORD,
+    // database: DB_NAME,
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "root",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "sourcedb",
+    dialect: "mysql",
+    // define: {
+    //   freezeTableName: true
+    // },
+    logging: false,
+    waitForConnections: true,
+    connectionLimit: process.env.DB_CONNECTION_LIMIT ? parseInt(process.env.DB_CONNECTION_LIMIT) : 10,
+    connectTimeout: 60000,
+    queueLimit: 0,
+    ssl: {
+      ca: fs.readFileSync(path.resolve(__dirname, "../../ca.pem"))
+    }
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USER,
+    // password: DB_PASSWORD,
+    // database: DB_NAME,
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "root",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "sourcedb",
+    dialect: "mysql",
+    // define: {
+    //   freezeTableName: true
+    // },
+    logging: false,
+    waitForConnections: true,
+    connectionLimit: process.env.DB_CONNECTION_LIMIT ? parseInt(process.env.DB_CONNECTION_LIMIT) : 10,
+    connectTimeout: 60000,
+    queueLimit: 0,
+    ssl: {
+      ca: fs.readFileSync(path.resolve(__dirname, "../../ca.pem"))
+    }
   }
 };
