7a7236f51b59ef6223178f6825c8c9db9ba19041
Tran Minh Thuan <tranminhthuan7230@gmail.com>
2024-12-19 23:59:33 +0700

    cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,24 +3,27 @@ require("dotenv").config();
 const configCors = (app) => {
     // Add headers before the routes are defined
     app.use(function (req, res, next) {
-        //console.log("CHeck method",req.method)
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+        // Website you wish to allow to connect (allow all origins)
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
         res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Handle preflight request
         if (req.method === "OPTIONS") {
-            return res.sendStatus(200)
+            return res.sendStatus(200);
         }
+
         // Pass to next layer of middleware
         next();
     });
-}
-export default configCors
+};
+
+export default configCors;
